262dbcbf7a73b79c9c61f00c115cb97db5a01d92
Vitaliy Gerassimov <102645214+VitaliyDeveloper@users.noreply.github.com>
2022-12-30 14:52:01 +0600

    reducer persist

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,15 +1,34 @@
-// import { createStore, combineReducers } from 'redux';
-// import { contactsReducer } from './contacts/contacts-reducers';
-
-// const rootReducer = combineReducers({ contacts: contactsReducer });
-
-// export const store = createStore(rootReducer);
-
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import contactsReducer from './contacts/contactsSlice';
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import { store } from 'components/redux/store';
+import { persistor } from 'components/redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="goit-react-hw-06-phonebook">
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter basename="goit-react-hw-06-phonebook">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
