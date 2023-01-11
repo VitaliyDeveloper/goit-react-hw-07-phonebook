461218221e28032088fb235328d583f8b0437bd2
Vitaliy Gerassimov <102645214+VitaliyDeveloper@users.noreply.github.com>
2023-01-11 09:49:28 +0600

    1

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter basename="goit-react-hw-06-phonebook">
+        <BrowserRouter basename="goit-react-hw-07-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
